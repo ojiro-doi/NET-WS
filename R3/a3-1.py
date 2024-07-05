0b0672a7b49a287ef7cdd7fd61edbfa0bc3ff5e0
ojiro-doi <sakura.0046@icloud.com>
2024-07-05 12:18:18 +0900

    fixed a3-1

--- R3/a3-1.py
+++ R3/a3-1.py
@@ -7,20 +7,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def route():
+
     dt = datetime.now()
 
-    if dt.second % 2 == 0:
-        color = "blue"
-    else:
-        color = "red"
-
     return render_template(
-        "a3-1.html",
-        title="A3-1",
-        hour=dt.hour,
-        minute=dt.minute,
-        second=dt.second,
-        color=color,
+        "a3-1.html", title="A3-1", hour=dt.hour, minute=dt.minute, second=dt.second
     )
 
 
